2c8ff86e1bfd7399042cb692adf14e09ae9b97b9
Bernardo Clepf <bernardo.clepf@alunos.unis.edu.br>
2024-09-26 14:36:54 -0300

    Adicionadas classes construtoras das tabelas do banco de dados.

---
+++ RUBS/Models/cidade.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+
+namespace RUBS
+{
+    public class cidade
+    {
+        [PrimaryKey, NotNull, AutoIncrement]
+        public int ID { get; set; }
+        [NotNull]
+        public string municipio { get; set; }
+        [NotNull]
+        public string codigo_municipio { get; set; }
+    }
+}
---
+++ RUBS/Models/estaberlecimentos.cs
@@ -0,0 +1,25 @@
+﻿using SQLite;
+
+
+namespace RUBS
+{
+    public class estabelecimentos
+    {
+        [PrimaryKey, NotNull]
+        public long codigo_cnes {  get; set; }
+        [NotNull]
+        public string nome_fantasia {  get; set; }
+        [NotNull]
+        public string endereco_estabelecimento {  get; set; }
+        [NotNull]
+        public string numero_estabelecimento { get; set; }
+        [NotNull]
+        public string bairro_estabelecimento { get; set; }
+        [NotNull]
+        public string descricao_turno_atendimento { get; set; }
+        [NotNull]
+        public double latitude_estabelecimento_decimo_grau { get; set; }
+        [NotNull]
+        public double longitude_estabelecimento_decimo_grau { get; set; }
+    }
+}
