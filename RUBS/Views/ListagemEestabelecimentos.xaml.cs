a5f89b17ec25b55d35d1522c7362057e7a7ef7d5
Bernardo Clepf <bernardo.clepf@alunos.unis.edu.br>
2024-10-01 18:23:30 -0300

    Ajustes nas páginas

---
+++ RUBS/Views/ListagemEestabelecimentos.xaml.cs
@@ -0,0 +1,42 @@
+namespace RUBS.Views;
+using RUBS.Services;
+public partial class ListagemEestabelecimentos : ContentPage
+{
+    private readonly ApiService _apiService;
+    public ListagemEestabelecimentos()
+    {
+        InitializeComponent();
+        _apiService = new ApiService();
+    }
+
+    // Quando o usuário clicar no botão, esse método será chamado
+    private async void BuscarEstabelecimentosClicked(object sender, EventArgs e)
+    {
+        string codigoMunicipio = CodigoMunicipioEntry.Text;
+
+        if (!string.IsNullOrEmpty(codigoMunicipio))
+        {
+            try
+            {
+                // Realiza a busca paginada
+                await _apiService.GetEstabelecimentosPaginadosAsync(codigoMunicipio);
+
+                // Carregar estabelecimentos salvos no banco e exibir na lista
+                var estabelecimentos = await _apiService._databaseService.GetEstabelecimentosAsync();
+
+                if (estabelecimentos != null)
+                {
+                    ListaEstabelecimentos.ItemsSource = estabelecimentos;
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro", ex.Message, "OK");
+            }
+        }
+        else
+        {
+            await DisplayAlert("Erro", "Por favor, insira o código do município", "OK");
+        }
+    }
+}
--- RUBS/Views/MainPage.xaml.cs
+++ RUBS/Views/MainPage.xaml.cs
@@ -5,37 +5,6 @@ public partial class MainPage : ContentPage
     public MainPage()
     {
         InitializeComponent();
-        _apiService = new ApiService();
     }
 
-    // Quando o usuário clicar no botão, esse método será chamado
-    private async void BuscarEstabelecimentosClicked(object sender, EventArgs e)
-    {
-        string codigoMunicipio = CodigoMunicipioEntry.Text;
-
-        if (!string.IsNullOrEmpty(codigoMunicipio))
-        {
-            try
-            {
-                // Realiza a busca paginada
-                await _apiService.GetEstabelecimentosPaginadosAsync(codigoMunicipio);
-
-                // Carregar estabelecimentos salvos no banco e exibir na lista
-                var estabelecimentos = await _apiService._databaseService.GetEstabelecimentosAsync();
-
-                if (estabelecimentos != null)
-                {
-                    ListaEstabelecimentos.ItemsSource = estabelecimentos;
-                }
-            }
-            catch (Exception ex)
-            {
-                await DisplayAlert("Erro", ex.Message, "OK");
-            }
-        }
-        else
-        {
-            await DisplayAlert("Erro", "Por favor, insira o código do município", "OK");
-        }
-    }
 }
